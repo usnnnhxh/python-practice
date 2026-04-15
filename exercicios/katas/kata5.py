b34640fb2981de3a6a658e0b65a38dfaba7cc987
luan <luan.ronzoni@hotmail.com>
2026-04-14 21:34:37 -0300

    daily exercise

---
+++ exercicios/katas/kata5.py
@@ -0,0 +1,8 @@
+def twoSum(nums, target):
+    for i in range(len(nums)):
+        for j in range(i + 1, len(nums)):
+            if nums[i] + nums[j] == target:
+                return True
+            
+    return False
+print(twoSum([1, 2, 3], 0))
